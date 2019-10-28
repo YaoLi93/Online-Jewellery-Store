b4ac14c73ec9534fa2e4e01aca21f3f8c314713b
Yao Li <43403868+YaoLi93@users.noreply.github.com>
2019-10-28 18:26:50 -0500

    Add files via upload

---
+++ Royal jewelry/js/script.js
@@ -0,0 +1,33 @@
+(function ($) { 'use strict';
+	
+
+  // Homepage Slider
+  $(document).ready(function(){
+    $('.home-slider').slick();
+  });
+
+
+
+
+
+
+})(jQuery);
+
+
+
+ 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
