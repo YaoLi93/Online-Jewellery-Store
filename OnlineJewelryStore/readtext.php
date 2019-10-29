656d53c914a057753e208b009ad46683be746298
Zehong <TheZhuangPark@outlook.com>
2019-10-28 22:09:38 -0500

    login

--- OnlineJewelryStore/readtext.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = $_POST['Jewelry_id'];
-$path='inventory/synopsis/'.$id.'.txt';
-$synopsis=file_get_contents($path);
-echo $synopsis;
-?>
---
+++ Royal jewelry/login.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+<html class="no-js"> <!--<![endif]-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Aviato E-Commerce Template">
+  
+  <meta name="author" content="">
+
+  <title>ROYAL JEWELRY</title>
+
+
+  <!-- Mobile Specific Meta-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+  
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="plugins/themefisher-font/style.css">
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="plugins/slick-carousel/slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="plugins/slick-carousel/slick/slick-theme.css"/>
+  
+
+  <!-- boostrap -->
+   <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style1.css">
+   <link rel="stylesheet" href="css/login.css">
+
+
+</head>
+
+<body id="body">
+
+
+ <!-- Pre Header -->
+    <div id="pre-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <span>FREE STANDARD SHIPPING OVER $ 75</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<!-- Start Top Header Bar -->
+<section class="top-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-xs-12 col-sm-4">
+				<div class="contact-number">
+					<i class="tf-ion-ios-telephone"></i>
+					<span>(202) 495-2266</span>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-12 col-sm-4">
+				<!-- Site Logo -->
+				<div class="logo text-center">
+					<a href="index.html">
+						<!-- replace logo here -->
+						<svg width="135px" height="29px" viewBox="0 0 155 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+						    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="40" font-family="AustinBold, Austin" font-weight="bold">
+						        <g id="Group" transform="translate(-108.000000, -297.000000)" fill="#000000">
+						        	<a id="AVIATO" class="navbar-brand" href="#"><img src="images/header-logo.png" alt=""></a>
+						        </g>
+						    </g>
+						</svg>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-12 col-sm-4">
+
+			<!-- Cart -->
+			<ul class="top-menu text-right list-inline">
+	          <li class="dropdown cart-nav dropdown-slide">
+	            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"><i class="tf-ion-android-cart"></i>Cart</a>
+	            <div class="dropdown-menu cart-dropdown">
+	            	<!-- Cart Item -->
+	            	<div class="media">
+	            		<a class="pull-left" href="#">
+	            			<img  class="media-object" src="images/shop/cart/cart-1.jpg" alt="image" />
+	            		</a>
+	            		<div class="media-body">
+	            			<h4 class="media-heading"><a href="">Bracelet</a></h4>
+	            			<div class="cart-price">
+                              <span>1 x</span>
+                              <span>125.00</span>
+                            </div>
+                            <h5><strong>$125</strong></h5>
+	            		</div>
+                        <a href="#" class="remove"><i class="tf-ion-close"></i></a>
+	            	</div><!-- / Cart Item -->
+	            	<!-- Cart Item -->
+	            	<div class="media">
+	            		<a class="pull-left" href="#">
+	            			<img  class="media-object" src="images/shop/cart/cart-2.jpg" alt="image" />
+	            		</a>
+	            		<div class="media-body">
+	            			<h4 class="media-heading"><a href="">Ladies Bag</a></h4>
+	            			<div class="cart-price">
+                              <span>1 x</span>
+                              <span>70.00</span>
+                            </div>
+                            <h5><strong>$70</strong></h5>
+	            		</div>
+                        <a href="#" class="remove"><i class="tf-ion-close"></i></a>
+	            	</div><!-- / Cart Item -->
+	            	
+	            	<div class="cart-summary">
+                        <span>Total</span>
+                        <span class="total-price">$195.00</span>
+                    </div>
+                    <ul class="text-center cart-buttons">
+                    	<li><a href="" class="btn btn-small btn-solid-border">View Cart</a></li>
+                    	<li><a href="" class="btn btn-small btn-solid-border">Checkout</a></li>
+                    </ul>
+                </div>
+
+	          </li><!-- / Cart -->
+
+	          <!-- Search -->
+	          <li class="dropdown search dropdown-slide">
+	            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"><i class="tf-ion-ios-search-strong"></i> Search</a>
+	            <ul class="dropdown-menu search-dropdown">
+	             <li><form action="post"><input type="search" class="form-control" placeholder="Search..."></form></li>
+	            </ul>
+
+	          </li><!-- / Search -->
+
+	          <!-- Languages -->
+	          <li class="commonSelect">
+	          	<select class="form-control">
+                    <option>EN</option>
+                    <option>CH</option>
+
+                </select>
+	          </li><!-- / Languages -->
+
+
+	          <!-- Log in -->
+	          <li class="login">
+	        	<a href="">|</a>
+                <a href="#">Log in</a>
+	          </li><!-- / Log in -->
+
+	        </ul><!-- / .nav .navbar-nav .navbar-right -->
+			</div>
+		</div>
+	</div>
+</section><!-- End Top Header Bar -->
+
+
+<!-- Main Menu Section -->
+<nav class="navbar navigation">
+	    <div class="container">
+
+	      <!-- Navbar Links -->
+	      <div id="navbar" class="navbar-collapse collapse text-center">
+	        <ul class="nav navbar-nav">
+
+	          <!-- Home -->
+	          <li class="dropdown ">
+	            <a href="index.html">Home</a>
+	          </li><!-- Home -->
+	          <!-- Produts -->
+	          <li>
+	            <a href="shop.html">Produts</a>
+	          </li><!-- Produts -->
+	          <!-- Contact -->
+	          <li>
+	            <a href="contact.html">Contact</a>
+	          </li><!-- Contact -->
+
+
+	          <!-- Blog -->
+	          <li class="dropdown dropdown-slide">
+	            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="350" role="button" aria-haspopup="true" aria-expanded="false">About Us <span class="tf-ion-ios-arrow-down"></span></a>
+	            <ul class="dropdown-menu">
+	            	<li><a href="#">Developers</a></li>
+					<li><a href="#">Newsletter</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Other ROYAL JEWELRY sites</a></li>
+	            </ul>
+	          </li><!-- / Blog -->
+
+	        </ul><!-- / .nav .navbar-nav -->
+	      	</div><!--/.navbar-collapse -->
+	    </div><!-- / .container -->
+	</nav>
+
+
+<!-- welcome h1 -->
+	<h1 class="welcome">WELCOME</h1>
+
+<!-- login -->
+<form>
+<div class="container">
+       	 <div class="row">
+          	<div class="col" style="margin-left: 0%">
+          		<h3 class="welcome">Login</h3>
+
+				  	<div class="form-group">
+				    <label for="exampleInputEmail1">Email address</label>
+				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Password</label>
+				    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				</div></div>
+
+			
+				
+          <div class="col">
+           	<h3 class="welcome">Register</h1>
+           		<p>
+           			<br>
+          		Need a new account for your online jwelery shopping?<br>
+          	  	✔️ Get access to limited edition products<br>
+				✔️ Level up for a Runtastic Premium subscription<br>
+				✔️ Faster checkout with your saved address and payment details<br>
+				✔️ Access your complete order history<br>
+          		</p>
+          </div>
+         
+        </div>
+
+        <br><br><br>
+         <div class="row">
+         	 <div class="col"> 
+         	 	  <button type="submit" class="btn btn-dark btn-lg btn-block">Login</button>
+         	 	</div>
+         	  <div class="col"> 
+         	  	  <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
+         	  	</div>
+      </div>
+	</form>
+	<br><br><br>
+
+
+
+<!--=====Footer======-->
+<!--=====Footer======-->
+<footer class="footer section text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="social-media">
+					<li>
+						<a href="#">
+							<i class="tf-ion-social-facebook"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="tf-ion-social-instagram"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="tf-ion-social-twitter"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="tf-ion-social-pinterest"></i>
+						</a>
+					</li>
+				</ul>
+				<ul class="footer-menu">
+					<li>
+						<a href="#">CONTACT</a>
+					</li>
+					<li>
+						<a href="#">SHIPPING</a>
+					</li>
+					<li>
+						<a href="#">TERMS OF SERVICE</a>
+					</li>
+					<li>
+						<a href="#">PRIVACY POLICY</a>
+					</li>
+				</ul>
+				<p class="copyright-text">Copyright © 2019 ROYAL JEWELRY. All rights reserved <br> Designed by Yao Li AAA BBB </p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    
+
+    <!-- Main jQuery -->
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js" async></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="plugins/bootstrap/js/bootstrap.min.js" async></script>
+  
+    <!-- Slick Carousel -->
+    <script src="plugins/slick-carousel/slick/slick.min.js" async></script>
+   
+
+    <!-- Main Js File -->
+    <!-- <script src="js/script.js"></script> -->
+    
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+
+  </body>
+  </html>
+
+
+
+<?php
+//echo "login page";
+
+
+?>
