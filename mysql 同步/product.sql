fbb28440ad03f7e264ca6e54941c58d491405c1b
Yao Li <43403868+YaoLi93@users.noreply.github.com>
2019-11-29 03:52:44 -0600

    Add files via upload

---
+++ mysql 同步/product.sql
@@ -0,0 +1,75 @@
+INSERT INTO product
+VALUES (1001,'SOMMERSET EARRINGS','36.50 $','3','This classic pair of earrings is easy to wear on any occasion. The outer edge of each rhodium-plated hoop is delicately embellished with clear crystal pavé, lending a soft sparkle to this timeless design.','1');
+
+INSERT INTO product
+VALUES(1002,'ANGELIC PIERCED EARRINGS','59.00 $','3','A classic pair of earrings can enhance just about any outfit. This pair of rhodium-plated button pierced earrings does just that! Each piece displays a single clear crystal chaton framed in clear crystal pavé, creating a beautiful sparkle. The earrings come as a pair.','1');
+
+INSERT INTO product
+VALUES(1003,'TAROT MAGIC STUD PIERCED EARRINGS','59.00 $','3','The Tarot Magic jewelry family is inspired by the mystical world of tarot symbols. This must-have pair of earrings elegantly combines crystal pavé with a gold-tone plated spade design. Perfect for adding a touch of sparkle and intrigue to any outfit.','1');
+
+INSERT INTO product
+VALUES(1004,'SOLITAIRE PIERCED EARRINGS','51.5 $','3','Each stud of the Solitaire pierced earrings comprises one crystal solitaire in an elegant setting. This pair of rhodium-plated earrings dazzles and shimmers in subtle ways, making a statement of classic, minimalistic elegance. The earrings come as a pair and combine perfectly with Swarovski’s other clear crystal designs.','1');
+
+INSERT INTO product
+VALUES(1005,'BELLA EARRINGS','51.5 $','3','Add sparkle to your day to day outfit with this pair of rhodium-plated pierced earrings! Casual and glamorous, they shine in clear crystal and suit any occasion. Diameter of the crystal: 1.1 cm','1');
+
+INSERT INTO product
+VALUES(1006,'TAROT MAGIC PIERCED EARRINGS','96.50 $', '3','The Tarot Magic jewelry family is inspired by the mystical world of tarot symbols. This must-have pair of pierced earrings brings together a fascinating collection of meaningful motifs, including a delicate swallow, a key, and hands. Full of intriguing details, the gold-tone plated design features stunning crystals in hues of blue. A real talking piece.','1');
+
+INSERT INTO product
+VALUES(1007,'CREATIVITY CIRCLE PIERCED EARRINGS','44.00 $','3','Delicate circle silhouettes inject timeless elegance into any look, and this refined design adds a beautiful sparkle too. The metal is rhodium-plated, so will not tarnish. Easy to combine with other Swarovski jewelry, the earrings come as a pair and measure 1 cm.','1');
+
+INSERT INTO product
+VALUES(1008,'NICE PIERCED EARRINGS','74.00 $','3','Feathers are the must-have motifs of the season, and this elegant pair of pierced earrings offers a very refined and easy-to-wear spin on the trend. Exquisitely crafted with Crystal Pearls, they will infuse your party season outfits with classic rhodium plated glamour.','1');
+
+INSERT INTO product
+VALUES(2001,'LIFELONG BOW Y NECKLACE','96.50 $','3','The Lifelong Bow collection uses the bow motif as a flirtatious and fun symbol of love. Sophisticated yet playful and youthful, this necklace exudes cool glamour with its sleek lines, 3D silhouette, and mix of rose gold and rhodium plating. Perfect for thoughtful gift-givers, or for treating yourself.','1');
+
+INSERT INTO product
+VALUES(2002,'ATTRACT NECKLACE','51.50 $','3','Keep it chic and simple with the Attract Necklace. Timeless and elegant, the rhodium plated design features a dazzling square-cut stone. Ideal for any occasion, it is a must-have staple for any jewelry collection and would make a great gift. Easy to mix and match with other Swarovski jewelry pieces.','1');
+
+INSERT INTO product
+VALUES(2003,'TAROT MAGIC CHARM PENDANT','111.50 $','3','The Tarot Magic jewelry family is inspired by the mystical world of tarot symbols. This must-have gold-tone plated pendant brings together a fascinating collection of meaningful motifs, including a delicate swallow, a key, and a hand. Featuring stunning blue-colored crystals, it is full of intriguing details and will add conversation-starting style to any outfit. The pendant comes on a chain','1');
+
+INSERT INTO product
+VALUES(2004,'SPARKLING DANCE ROUND NECKLACE','96.50 $','3','Inspired by the idea of a ‘dancing crystal’, this necklace showcases a round stone that floats inside a metal cage embellished with crystal pavé. An essential addition to any jewelry box, it will give your daytime style a playful sparkle. The necklace is rhodium-plated','1');
+
+INSERT INTO product
+VALUES(2005,'TAROT MAGIC PENDANT','74.00 $','3','The Tarot Magic jewelry family is inspired by the mystical world of tarot symbols. This intriguing pendant on a gold-tone plated chain features a delicate hand symbol, representing power, protection, and blessings, as well as a gray crystal and clear pavé for extra sparkle. Layer with other styles for an on-trend look.','1');
+
+INSERT INTO product
+VALUES(2006,'SYMBOLIC MOON NECKLACE SET','96.50 $','3','A great gift idea, or an elegant everyday essential for your own collection, this playful set of two necklaces can be layered together or worn separately. The design combines different metal platings and both chains are peppered with clear and colored crystals. A delicate metal star and a pavé-embellished moon dangle from the longer chain.','1');
+
+INSERT INTO product
+VALUES(2007,'PARKLING DANCE ROUND NECKLACE','96.50 $','3','Featuring a round dark red stone inside a pavé-embellished 3D cage, this sophisticated rose gold-plated necklace is inspired by the idea of a ‘dancing crystal’. An essential addition to any jewelry box, it will give your daytime style a playful sparkle.','1');
+
+INSERT INTO product
+VALUES(2008,'OUT OF THIS WORLD UNICORN NECKLACE','66.50 $','3','Feel like a fairytale princess in this online-exclusive unicorn necklace. Crafted in gold-tone plated metal, it features colorful, crystal-embellished unicorn motifs. Perfect for adding a playful touch to your everyday outfits, and a great gift idea','1');
+
+INSERT INTO product
+VALUES(3001,'VITTORE XL RING','66.50 $','3','A wider version of our bestselling Vittore Ring. Sparkling on all sides, this gorgeous rhodium-plated accessory is embellished with clear stones. Sure to add instant glamour to your hand, this classic design is understated and sophisticated. Wear two or three together for extra impact or stack with other styles to elevate your look.','1');
+
+INSERT INTO product
+VALUES(3002,'ATTRACT RING SET','93.50 $','3','Add a touch of sparkle to your fingers with these two rhodium-plated rings. Embellished with sparkling chatons, the rings look great whether worn together or separately. The perfect romantic gift.','1');
+
+INSERT INTO product
+VALUES(3003,'ATTRACT ROUND RING','66.50 $','3',' Attract Round ring is a truly romantic gift. This stunning, rhodium-plated ring features a round chaton in the center with sparkling chatons on either side. The chatons embellish the front half of the ring. The perfect jewelry piece for day or evening outfits!','1');
+
+INSERT INTO product
+VALUES(3004,'NICE MOTIF RING','89.00 $','3','Feathers are the must-have motifs of the season, and this elegant gold-tone plated ring offers a very refined and easy-to-wear spin on the trend. Exquisitely crafted, it will add polished chic to your fall/winter outfits and beyond. Mix and match with contrasting rings for an on-trend look.','1');
+
+INSERT INTO product
+VALUES(3005,'ANGELIC RING','74.00 $','3',' A luxurious extension of our well-loved Angelic collection, it features a band of circular motifs. Crafted in rhodium-plated metal with clear crystal centerpieces nestled in clear pavé, it is timelessly elegant. A classic and sophisticated gift for a loved one, whatever the occasion','1');
+
+INSERT INTO product
+VALUES(3006,'PALACE MOTIF RING','96.50 $','3','Full of vintage glamour, this timeless rhodium plated ring will fit seamlessly into any collection. Featuring a regal blue crystal centerpiece, framed with abstract floral shapes in sumptuous sparkling stones, it is an elegant and refined design. Matching items are available.','1');
+
+INSERT INTO product
+VALUES(3007,'NORTH MOTIF RING','89.00 $','3','Add timeless polish to any outfit with this on-trend rose-gold tone plated ring. The banded open-shank design features a frosting of sparkling clear crystal pavé. A fashion forward accessory with classic sparkle.','1');
+
+INSERT INTO product
+VALUES(3008,'NAUGHTY MOTIF RING','66.50 $','3','With its on-trend feather motif and black crystal pavé, it will add a fashionable rose-gold tone plated touch to any day or evening ensemble. Mix and match with other styles to create your own ring party','1');
+
+
+
+
