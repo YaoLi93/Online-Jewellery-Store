898568f3192f216892dc100f545433755bddd8b2
Yao Li <43403868+YaoLi93@users.noreply.github.com>
2019-11-30 00:04:11 -0600

    Add files via upload

---
+++ mysql 同步/product_category.sql
@@ -0,0 +1,71 @@
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1001,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1002,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1003,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1004,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1005,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1006,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1007,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(1008,'earrings');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2001,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2002,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2003,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2004,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2005,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2006,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2007,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(2008,'necklace');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3001,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3002,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3003,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3004,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3005,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3006,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3007,'ring');
+
+INSERT INTO product_category(PID,CATEGORY)
+VALUES(3008,'ring');
---
+++ mysql 同步/product_image.sql
@@ -0,0 +1,71 @@
+INSERT INTO product_image
+VALUES(1001,'product_image/earring/1001_1.jpg','product_image/earring/1001_2.jpg','product_image/earring/1001_3.jpg');
+
+INSERT INTO product_image
+VALUES(1002,'product_image/earring/1002_1.jpg','product_image/earring/1002_2.jpg','product_image/earring/1002_3.jpg');
+
+INSERT INTO product_image
+VALUES(1003,'product_image/earring/1003_1.jpg','product_image/earring/1003_2.jpg','product_image/earring/1003_3.jpg');
+
+INSERT INTO product_image
+VALUES(1004,'product_image/earring/1004_1.jpg','product_image/earring/1004_2.jpg','product_image/earring/1004_3.jpg');
+
+INSERT INTO product_image
+VALUES(1005,'product_image/earring/1005_1.jpg','product_image/earring/1005_2.jpg','product_image/earring/1005_3.jpg');
+
+INSERT INTO product_image
+VALUES(1006,'product_image/earring/1006_1.jpg','product_image/earring/1006_2.jpg','product_image/earring/1006_3.jpg');
+
+INSERT INTO product_image
+VALUES(1007,'product_image/earring/1007_1.jpg','product_image/earring/1007_2.jpg','product_image/earring/1007_3.jpg');
+
+INSERT INTO product_image
+VALUES(1008,'product_image/earring/1008_1.jpg','product_image/earring/1008_2.jpg','product_image/earring/1008_3.jpg');
+
+INSERT INTO product_image
+VALUES(2001,'product_image/necklace/2001_1.jpg','product_image/necklace/2001_2.jpg','product_image/necklace/2001_3.jpg');
+
+INSERT INTO product_image
+VALUES(2002,'product_image/necklace/2002_1.jpg','product_image/necklace/2002_2.jpg','product_image/necklace/2002_3.jpg');
+
+INSERT INTO product_image
+VALUES(2003,'product_image/necklace/2003_1.jpg','product_image/necklace/2003_2.jpg','product_image/necklace/2003_3.jpg');
+
+INSERT INTO product_image
+VALUES(2004,'product_image/necklace/2004_1.jpg','product_image/necklace/2004_2.jpg','product_image/necklace/2004_3.jpg');
+
+INSERT INTO product_image
+VALUES(2005,'product_image/necklace/2005_1.jpg','product_image/necklace/2005_2.jpg','product_image/necklace/2005_3.jpg');
+
+INSERT INTO product_image
+VALUES(2006,'product_image/necklace/2006_1.jpg','product_image/necklace/2006_2.jpg','product_image/necklace/2006_3.jpg');
+
+INSERT INTO product_image
+VALUES(2007,'product_image/necklace/2007_1.jpg','product_image/necklace/2007_2.jpg','product_image/necklace/2007_3.jpg');
+
+INSERT INTO product_image
+VALUES(2008,'product_image/necklace/2008_1.jpg','product_image/necklace/2008_2.jpg','product_image/necklace/2008_3.jpg');
+
+INSERT INTO product_image
+VALUES(3001,'product_image/rings/3001_1.jpg','product_image/rings/3001_2.jpg','product_image/rings/3001_3.jpg');
+
+INSERT INTO product_image
+VALUES(3002,'product_image/rings/3002_1.jpg','product_image/rings/3002_2.jpg','product_image/rings/3002_3.jpg');
+
+INSERT INTO product_image
+VALUES(3003,'product_image/rings/3003_1.jpg','product_image/rings/3003_2.jpg','product_image/rings/3003_3.jpg');
+
+INSERT INTO product_image
+VALUES(3004,'product_image/rings/3004_1.jpg','product_image/rings/3004_2.jpg','product_image/rings/3004_3.jpg');
+
+INSERT INTO product_image
+VALUES(3005,'product_image/rings/3005_1.jpg','product_image/rings/3005_2.jpg','product_image/rings/3005_3.jpg');
+
+INSERT INTO product_image
+VALUES(3006,'product_image/rings/3006_1.jpg','product_image/rings/3006_2.jpg','product_image/rings/3006_3.jpg');
+
+INSERT INTO product_image
+VALUES(3007,'product_image/rings/3007_1.jpg','product_image/rings/3007_2.jpg','product_image/rings/3007_3.jpg');
+
+INSERT INTO product_image
+VALUES(3008,'product_image/rings/3008_1.jpg','product_image/rings/3008_2.jpg','product_image/rings/3008_3.jpg');
